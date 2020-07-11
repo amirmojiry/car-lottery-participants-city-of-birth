a435238d5fae75bb1a861946da3fd498639bd1c5
Amirhossein Mojiri Foroushani <amojiry@gmail.com>
2020-07-11 20:51:33 +0430

    reduce lines

--- check_possible_codes.php
+++ check_possible_codes.php
@@ -15,38 +15,31 @@ class NationalCode
         $this->control_digit = $number_array[9];
     }
 
+    private function sum_four_middle_digits($number) {
+        if ($number < 10) {
+            $number = '000'.$number;
+        }
+        elseif ($number < 100) {
+            $number = '00'.$number;
+        }
+        elseif ($number < 1000) {
+            $number = '0'.$number;
+        }
+        $number_array = str_split((string)$number);
+        return ($number_array[0] * 7) + ($number_array[1] * 6) 
+             + ($number_array[2] * 5) + ($number_array[3] * 4);
+    }
+
     public function count_valid_numbers() {
         $count_valid_numbers = 0;
         for ($i = 0; $i < 10000; $i++) {
-            $count_all_digits = $this->sum_four_middle_digits($i) + $this->number_we_have;
-            $remainder = $count_all_digits%11;
-            if ($remainder < 2 && $this->control_digit == $remainder 
+            $remainder = ($this->sum_four_middle_digits($i) + $this->number_we_have)%11;
+            if (($remainder < 2 && $this->control_digit == $remainder) 
                 || $this->control_digit == 11 - $remainder) {
                 $count_valid_numbers++;
             }
         }
         return $count_valid_numbers;
-    }
-
-    private function sum_four_middle_digits($number)
-    {
-        if ($number < 10) {
-            $sum = $number * 4;
-        }
-        elseif ($number < 100) {
-            $number_array = str_split((string)$number);
-            $sum = ($number_array[0] * 5) + ($number_array[1] * 4);
-        }
-        elseif ($number < 1000) {
-            $number_array = str_split((string)$number);
-            $sum = ($number_array[0] * 6) + ($number_array[1] * 5) + ($number_array[2] * 4);
-        }
-        else {
-            $number_array = str_split((string)$number);
-            $sum = ($number_array[0] * 7) + ($number_array[1] * 6) 
-                + ($number_array[2] * 5) + ($number_array[3] * 4);
-        }
-        return $sum;
     }
 }
 
